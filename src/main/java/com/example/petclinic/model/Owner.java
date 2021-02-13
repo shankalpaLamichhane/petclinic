06b375eb506577554e8ede0bc3a15794ff04fdf2
sankalpa <sankalpa.lamichhane@citytech.global>
2021-02-13 14:46:16 +0545

    models added

---
+++ src/main/java/com/example/petclinic/model/Owner.java
@@ -0,0 +1,8 @@
+package com.example.petclinic.model;
+
+/**
+ * @author sankalpa on 2/13/21
+ */
+public class Owner extends Person{
+
+}
---
+++ src/main/java/com/example/petclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package com.example.petclinic.model;
+
+/**
+ * @author sankalpa on 2/13/21
+ */
+public class Vet extends Person{
+}
