2475027ad5ec693320c0512de3f37d4fef554950
sankalpa <sankalpa.lamichhane@citytech.global>
2021-02-13 14:39:17 +0545

    added pom

---
+++ src/main/java/com.example.petclinic/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sankalpa on 2/13/21
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+}
